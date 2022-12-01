c9fe793144a385a2a8026a869c023a9164af68f0
SaiVenkat12 <gsaiv1001@gmail.com>
2022-12-01 13:05:45 +0530

    [SaiVenkat]-LeapYear

---
+++ Leapyear/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+namespace leapyear
+{
+    internal class program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the year :");
+            int x = int.Parse(Console.ReadLine());
+
+            if ((x >= 1000) && (x < 10000))
+            {
+                Console.WriteLine("{0} is a 4 digit number", x);
+                if ((x % 100 != 0) && (x % 4 == 0) || (x % 400 == 0))
+                {
+                    Console.WriteLine("{0} is a Leap Year", x);
+                }
+                else
+                {
+                    Console.WriteLine("{0} is Not a Leap Year", x);
+                }
+            }
+            else
+            {
+                Console.WriteLine("{0} is Not a 4 digit number", x);
+            }
+
+        }
+    }
+}
