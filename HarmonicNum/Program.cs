034647deaf1f9eb2f7a4b73012b4c0c3ff292dab
SaiVenkat12 <gsaiv1001@gmail.com>
2022-12-01 20:45:45 +0530

    [SaiVenkat]-Sum of Harmonic Number

---
+++ HarmonicNum/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+namespace HarmonicNum
+{
+    internal class program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the Nth Term of Harmonic : ");
+            int n = int.Parse(Console.ReadLine());
+            double harmonic = 0;
+            if (n != 0)
+            {
+                for (int i = 1; i <=n; i++)
+                {
+                    harmonic += (float)1 / i;
+                }
+                Console.WriteLine("The Sum of Nth term in Harmonic Series is" + harmonic);
+            }
+            else
+            {
+                Console.WriteLine("The term {0} is incorrect", n);
+            }
+        }
+    }
+}
