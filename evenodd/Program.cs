c3419e902e7e39084e1f1a657ac9496cc0b07315
SaiVenkat12 <gsaiv1001@gmail.com>
2022-12-01 13:00:23 +0530

    [SaiVenkat]-EvenOdd

---
+++ evenodd/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace evenodd
+{
+    internal class program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the Number ");
+            int x = int.Parse(Console.ReadLine());
+            if (x % 2 == 0)
+            {
+                Console.WriteLine("{0} is a Even Nummber", x);
+            }
+            else
+            {
+                Console.WriteLine("{0} is a Odd Number", x);
+            }
+        }
+    }
+}
