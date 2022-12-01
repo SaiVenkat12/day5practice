75995dda54442e00ef516d3cd2a5066b9a0f2c2c
SaiVenkat12 <gsaiv1001@gmail.com>
2022-12-01 13:31:23 +0530

    [SaiVenkat]-Greatest Number

---
+++ largestnum/Program.cs
@@ -0,0 +1,36 @@
+﻿
+using System;
+namespace consoleapp1
+{
+    internal class program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the 1st Number ");
+            int x = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter the 2nd Number ");
+            int y = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter the 3rd Number ");
+            int z = int.Parse(Console.ReadLine());
+            if (x > y)
+            {
+                if (x > z)
+                {
+                    Console.WriteLine("{0} is greatest ", x);
+                }
+                else
+                {
+                    Console.WriteLine("{0} is greatest", z);
+                }
+            }
+            else if (y > z)
+            {
+                Console.WriteLine("{0} is greatest", y);
+            }
+            else
+            { Console.WriteLine("{0} is greatest", z);
+            }
+            
+        }
+    }
+}
