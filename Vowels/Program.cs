5b73cc253d69116dbd02a65c896fa5c70c95bb74
SaiVenkat12 <gsaiv1001@gmail.com>
2022-12-01 20:11:51 +0530

    [SaiVenkat]-Check vowel or Consonent

---
+++ Vowels/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+namespace consoleapp1
+{
+    internal class program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the Alphabet :");
+            char c1= char.Parse(Console.ReadLine());
+            if (c1 == 'a' || c1 == 'A')
+            {
+                Console.WriteLine(c1 + " is a Vowel");
+            }
+            else if (c1 == 'e' || c1 == 'E')
+            {
+                Console.WriteLine(c1 + " is a Vowel");
+            }
+            else if (c1 == 'i' || c1 == 'I')
+            {
+                Console.WriteLine(c1 + " is a Vowel");
+            }
+            else if (c1 == 'o' || c1 == 'O')
+            {
+                Console.WriteLine(c1 + " is a Vowel");
+            }
+            else if (c1 == 'u' || c1 == 'U')
+            {
+                Console.WriteLine(c1 + " is a Vowel");
+            }
+            else 
+            {
+                Console.WriteLine(c1 + " is a Consonent");
+            }
+        }
+    }
+}
