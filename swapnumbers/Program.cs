e24d3b15c107954a5c8e52bf95a313628a23ca39
SaiVenkat12 <gsaiv1001@gmail.com>
2022-12-01 14:55:42 +0530

    [SaiVenkat]-SwapNumbers

---
+++ swapnumbers/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace swapnumbers
+{
+    internal class program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the value of A :");
+            int a = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter the value of B :");
+            int b = int.Parse(Console.ReadLine());
+            int temp = 0;
+            temp= a;
+            a= b;
+            b= temp;
+            Console.WriteLine("The Values after Swap");
+            Console.WriteLine("The value of A = " + a);
+            Console.WriteLine("The value of B = " + b);
+
+        }
+    }
+}
